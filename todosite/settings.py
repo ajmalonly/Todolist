cb694c818449ed79e9a2d8e2accb9c2d335617dd
ajmal rambocus <ajmalrambocus@gmail.com>
2023-01-17 15:52:53 +0400

    done

--- todosite/settings.py
+++ todosite/settings.py
@@ -20,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
-
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-7vk^%hdv*ldrs21$y0(djxm9+7_lf5qawv!njjldl4zq+9ktm-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
